1e4c3f24634e6255d011a79ce7a314664da4cc0a
Scott <smerritt987@gmail.com>
2018-11-22 13:13:19 -0800

    finally fixed routing issue

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'login', pathMatch:'full'},
+      { path: 'home', component: HomeComponent},
+      { path: 'login', component: LoginComponent},
+      { path: 'profile', component: ProfileComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
